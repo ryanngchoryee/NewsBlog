ff6362eb9ce7952985bc9f6f3fe393936924defd
ryanngchoryee <ryanng0127@gmail.com>
2023-10-15 23:25:39 +0800

    Home view components structure

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
-import { Navbar } from 'react-bootstrap'
+import React, { useEffect, useState } from 'react'
+import Navigation from '../components/navbar'
 import Container from 'react-bootstrap/esm/Container'
+import { useNewsProvider } from '../providers/news-context';
+import { News } from '../models/news';
+import NewsCarousel from '../components/news-carousel';
 
 const Home: React.FC = () => {
+    const { getLatestNews, setTitleFilter } = useNewsProvider();
+    const [filteredNews, setFilteredNews] = useState<News[]>([]);
+
+    const onTitleFilterChange = (newTitleFilter: string) => {
+        setTitleFilter(newTitleFilter);
+    };
+
+    useEffect(() => {
+        //Update the news list when the titleFilter changes
+        const fetchNews = async () => {
+            const data = await getLatestNews();
+            if (data) {
+                //Update the filtered news based on the titleFilter
+                setFilteredNews(data.results);
+            }
+        };
+        fetchNews();
+    }, [getLatestNews]);
+
     return (
-        <Container fluid className='text-center'>
-            <h1>Welcome to NewsBlog!</h1>
-            {/* <Navbar /> */}
+        <Container fluid className='p-0' >
+            <Navigation onTitleFilterChange={onTitleFilterChange} />
+            <NewsCarousel news={filteredNews} />
         </Container>
     )
 }
